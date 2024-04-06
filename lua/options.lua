ef429e46a99dba0d994409b780c9c57eb177a9df
ludovite <ludovite@disroot.org>
2024-04-06 21:50:00 +0200

    vyrrho1

--- lua/options.lua
+++ lua/options.lua
@@ -1,62 +1,60 @@
 -- [[ Setting options ]]
 -- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
+-- NOTE: You can change these options as you wish!,
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.loader.enable()
+local options = {
+  backup = false, -- Creates a backup file
+  swapfile = false, -- Creates a swapfile
+  clipboard = 'unnamedplus', -- Remove this option if you want your OS clipboard to remain independent. See `:help 'clipboard'`
+  cmdheight = 2, -- More space in the neovim command line for displaying messages
+  conceallevel = 0, -- So that `` is visible in markdown files
+  fileencoding = 'utf-8', -- The encoding written to a file
+  hlsearch = true, -- Highlight all matches on previous search pattern
+  ignorecase = true, -- Ignore case in search patterns
+  smartcase = true,
+  smartindent = true, -- Make indenting smarter again
+  incsearch = true,
+  mousemoveevent = true,
+  mouse = 'a', -- Allow the mouse to be used in neovim
+  pumheight = 10, -- Pop up menu height
+  showmode = false, -- Don't show the mode, (already in the status line)
+  showtabline = 2, -- Always show tabs
+  splitbelow = true, -- Force all horizontal splits to go below current window
+  splitright = true, -- Force all vertical splits to go to the right of current window
+  termguicolors = true, -- Set term gui colors (most terminals support this)
+  timeoutlen = 800, -- Time to wait for a mapped sequence to complete (in ms).Displays which-key popup sooner
+  number = true,
+  -- relativenumber = true,
+  numberwidth = 3, -- Set number column width to 3 {default 4}
+  signcolumn = 'no', -- Always show the sign column, otherwise it would shift the text each time
+  breakindent = true, -- Enable break indent
+  undofile = true, -- Save undo history
+  updatetime = 250, -- Faster completion (4000ms default)
+  -- Sets how neovim will display certain whitespace characters in the editor.
+  list = true, --  See `:help 'list'`
+  wrap = true, -- Display lines as one long line if false
+  linebreak = true, -- word wrapping
+  showbreak = '↪ ',
+  listchars = { tab = '» ', trail = '·', nbsp = '␣' }, -- see `:help 'listchars'`
+  inccommand = 'split', -- Preview substitutions live, as you type!
+  scrolloff = 999, -- Minimal number of screen lines to keep above and below the cursor
+  sidescrolloff = 8,
+  writebackup = false, -- If a file is being edited by another program (or was written to file while editing
+  -- with another program), it is not allowed to be edited
+  expandtab = true, -- Convert tabs to spaces
+  shiftwidth = 3, -- The number of spaces inserted for each indentation
+  tabstop = 3, -- Insert 3 spaces for a tab
+  softtabstop = 3, -- Insert 3 spaces for a tab
+  cursorline = true, -- Highlight the current line
+  guifont = 'monospace:h17', -- the font used in graphical neovim applications
+  titlestring = [[%F %h%m%r%w NVIM]],
+  title = true, -- Print active filename in window titlebar
+}
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- vim: ts=2 sts=2 sw=2 et
+-- vim: ts=2 sts=2 sw=2 et foldmethod=marker foldlevel=0
