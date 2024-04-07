c15116ac6947d0e0541ac4676d79d0a66986f2dc
Ludovic <ludovite@disroot.org>
2024-04-07 17:32:11 +0200

    Indent-blankline multicolor (support rainbow_delimiters)

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,7 +4,35 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    config = function()
+      local highlight = { --{{{1
+        'RainbowRed',
+        'RainbowYellow',
+        'RainbowBlue',
+        'RainbowOrange',
+        'RainbowGreen',
+        'RainbowViolet',
+        'RainbowCyan',
+      } -- 1}}}
+      local hooks = require 'ibl.hooks'
+      -- create the highlight groups in the highlight setup hook, so they are reset every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function() --{{{1
+        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
+        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
+        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
+        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
+        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
+        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+      end) -- 1}}}
+      require('ibl').setup {
+        indent = {
+          highlight = highlight,
+          char = '⎸',
+        },
+      }
+      vim.g.rainbow_delimiters = { highlight = highlight }
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et fdm=marker fdl=0
