8a9a107da9a6172337ea37e71b36e3f8d53a84e5
ludovite <ludovite@disroot.org>
2024-06-20 18:16:54 +0200

    Bug nvim-ufo corrigé

--- lua/custom/plugins/nvim-ufo.lua
+++ lua/custom/plugins/nvim-ufo.lua
@@ -9,8 +9,8 @@ return {
         config = function()
           local builtin = require 'statuscol.builtin'
           require('statuscol').setup {
-            -- foldfunc = "builtin",
-            -- setopt = true,
+            foldfunc = 'builtin',
+            setopt = true,
             relculright = true,
             segments = {
               { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
@@ -25,10 +25,12 @@ return {
       -- Fold options
       vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
       vim.o.foldcolumn = '1' -- '0' is not bad
-      vim.o.foldlevel = 50 -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 4
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 0
       vim.o.foldenable = true
-
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
       require('ufo').setup()
     end,
   },
